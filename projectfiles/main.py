a337fca60351bd9b898e6dd499cb0d169745533d
Wojciech Kucharski <wojtekkucharski105@gmail.com>
2020-11-26 00:29:20 +0100

    Commit

--- projectfiles/main.py
+++ projectfiles/main.py
@@ -1,13 +1,13 @@
 from client import *
 try:
-    game = client()
+    game = client() #try to create client object
 except Exception as e:
     print(e)
 
 running = True
 while running:
     try:
-        running = game.run()
+        running = game.run() #run client object till it returns False, which means that user clicked [X]
     except Exception as e:
         print(e)
 
